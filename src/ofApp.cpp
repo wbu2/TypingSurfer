c9765f7c96944b7062f3c33b313779ee26c23988
wbu2 <wbu2@illinois.edu>
2019-11-21 14:44:43 -0600

    Implemented some general app layout

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,15 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    
+    start_background.load("start.jpg");
+    game_background.load("game.jpg");
+    default_font.loadFont("joystix monospace.ttf", 70);
+    small_font.loadFont("joystix monospace.ttf", 40);
+    font.loadFont("joystix monospace.ttf", 20);
+    
+    gui.setup();
+    slider.setup("position", 500, 0, 100);
 }
 
 //--------------------------------------------------------------
@@ -12,12 +20,21 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    if(current_state == START_SCREEN){
+        drawGameStart();
+    }
+    if(current_state == RUNNING){
+        drawGameRunning();
+    }
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if(current_state == START_SCREEN && key == '1'){
+        current_state = RUNNING;
+    }
+     
 }
 
 //--------------------------------------------------------------
@@ -61,11 +78,16 @@ void ofApp::windowResized(int w, int h){
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
 
+void ofApp::drawGameStart(){
+    start_background.draw(0, 0, 1024, 768);
+    
+    default_font.drawString("Type Race", ofGetWindowWidth() / 6, ofGetWindowHeight() / 5);
+    small_font.drawString("Press 1 to Start", ofGetWindowWidth() / 6, ofGetWindowHeight() / 4);
 }
 
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::drawGameRunning(){
+    //game_background.draw(0, 0, 1024, 768);
+    
+    font.drawStringCentered("Table", ofGetWindowHeight()/4, ofGetWindowWidth()/ 3);
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,16 +1,34 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxCenteredTrueTypeFont.h"
+#inclue "ofxControlPlanel.h"
 
 class ofApp : public ofBaseApp{
     
     enum GameState{
         START_SCREEN = 0,
         RUNNING,
-        FINISHED
+        ENDED
     };
+private:
+    GameState current_state = START_SCREEN;
+    
+    ofImage start_background;
+    ofImage game_background;
+    
+    ofTrueTypeFont default_font;
+    ofTrueTypeFont small_font;
+    
+    ofxCenteredTrueTypeFont font;
+    
+    string user_input;
 
-	public:
+    
+    
+    
+
+public:
 		void setup();
 		void update();
 		void draw();
@@ -24,7 +42,7 @@ class ofApp : public ofBaseApp{
 		void mouseEntered(int x, int y);
 		void mouseExited(int x, int y);
 		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+    void drawGameStart();
+    void drawGameRunning();
 		
 };
